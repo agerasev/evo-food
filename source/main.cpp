e92f2bec63c2756e545c165e0f6d3999765c0422
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-09-14 17:49:02 +0700

    reorganize libworld2d

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,46 @@
 #include <QApplication>
 #include <QWidget>
+#include <QHBoxLayout>
+
+#include <world/world.hpp>
+
+#include <world/scene.hpp>
+#include <world/view.hpp>
+
 
 class Window : public QWidget {
 public:
-	Window() : QWidget() {
+	World &world;
+	
+	Scene scene;
+	View view;
+	
+	QHBoxLayout layout;
+	
+	Window(World &w) : QWidget(), 
+		world(w), scene(w), view(scene)
+	{
 		resize(1280, 720);
 		setWindowTitle("EvoFood");
+		
+		layout.addWidget(&view, 1);
+		
+		setLayout(&layout);
 	}
 };
 
 
 int main(int argc, char *argv[]) {
+	World world(vec2(10, 10));
+	world.add(new Entity());
+	
 	QApplication app(argc, argv);
 	
-	Window window;
+	Window window(world);
 	window.show();
+	
+	window.scene.sync();
+	window.scene.update();
 
 	int rs = app.exec();
 
